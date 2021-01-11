1538c99ce3fa9c92918da09c60570fb93fdc14fd
Mishamba <misha.nenahov@gmail.com>
2021-01-11 17:51:56 +0300

    Added TagController Created new CertificateController methods

--- src/main/java/com/epam/esm/controller/CertificateController.java
+++ src/main/java/com/epam/esm/controller/CertificateController.java
@@ -1,15 +1,39 @@
 package com.epam.esm.controller;
 
-import org.springframework.stereotype.Component;
+import com.epam.esm.model.entity.Certificate;
+import com.epam.esm.model.entity.Tag;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/certificate")
 public class CertificateController {
     @GetMapping
-    public String index() {
+    @ResponseBody
+    public List<Certificate> certificateIndex() {
 
     }
+
+    @GetMapping
+    @ResponseBody
+    public List<Tag> tagIndex() {
+
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Certificate getCertificate(@PathVariable("id") int id) {
+
+    }
+
+    // TODO: 1/11/21 add parameters
+    @PostMapping
+    @ResponseBody
+    public Boolean createCertificate() {
+
+    }
+
+    // TODO: 1/11/21 add other methods
 }
