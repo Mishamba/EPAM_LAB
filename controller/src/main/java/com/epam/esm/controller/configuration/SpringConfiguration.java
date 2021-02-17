f33b41d77eb767e8449373e678e3181d780261f9
Mishamba <misha.nenahov@gmail.com>
2021-02-17 16:21:33 +0300

    finished merge!

--- controller/src/main/java/com/epam/esm/controller/configuration/SpringConfiguration.java
+++ controller/src/main/java/com/epam/esm/controller/configuration/SpringConfiguration.java
@@ -1,53 +1,79 @@
-package com.epam.esm.controller.configuration;
+package com.epam.esm.controller.confugration;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.TransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
-import java.util.Objects;
+import java.nio.channels.Channel;
+import java.util.Locale;
+import java.util.Properties;
 
-@Configuration
-@EnableTransactionManagement
-@EnableWebMvc
-@ComponentScan("com.epam.esm")
-@PropertySource(value = "classpath:database-config.properties")
+@SpringBootConfiguration
 public class SpringConfiguration {
-
-    private final Environment environment;
+    private final Environment env;
 
     @Autowired
-    public SpringConfiguration(Environment environment) {
-        this.environment = environment;
+    public SpringConfiguration(Environment env) {
+        this.env = env;
     }
 
-    @Bean
-    public DataSource dataSource() {
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(environment.getProperty("database.url"));
-        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("database.driver")));
-        dataSource.setUsername(environment.getProperty("database.username"));
-        dataSource.setPassword(environment.getProperty("database.password"));
+
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        System.out.println("## getDataSource: " + dataSource);
 
         return dataSource;
     }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+
+        properties.put("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+        properties.put("current_session_context_class", //
+                env.getProperty("spring.jpa.properties.hibernate.current_session_context_class"));
+
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        factoryBean.setPackagesToScan("com.epam.esm.model.entity");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
     }
 
-    @Bean
-    public TransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        source.setDefaultLocale(new Locale("ru", "RU"));
+
+        return source;
     }
 }
